40b7a917bddfa317cc8602cfb1c4ff32b4b19749
RohitSenta <rohitsenta@gmail.com>
2023-07-27 17:57:13 +0530

    Numbers In JavaScript

---
+++ Numbers-In-JavaScript.js
@@ -0,0 +1,29 @@
+// Numbers In JavaScript
+
+console.log("Number In JavaScript \n\n");
+
+let x1 = 5;
+let y1 = 3.14;
+console.log("x =", x1, "\ny =", y1);
+console.log("\n");
+
+
+let x2 = 123e5;
+let y2 = 123e-5;
+console.log("x2 =", x2, "\ny2 =", y2);
+console.log("\n");
+
+
+let x3 = 999999999999999;
+let y3 = 9999999999999999;
+console.log("x3 =", x3, "\ny3 =", y3);
+console.log("\n");
+
+
+let x4 = 0.5;
+let y4 = 0.3;
+console.log("x4 =", x4, "\ny4 =", y4);
+console.log("x4 + y4 =", x4 + y4);  
+console.log("\n");
+
+
