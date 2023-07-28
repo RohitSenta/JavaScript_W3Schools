96ad1c05bb89738c9e5f298487cacd4aa78fe0f9
RohitSenta <rohitsenta@gmail.com>
2023-07-28 17:56:47 +0530

    Numbers In JavaScript

--- Numbers-In-JavaScript.js
+++ Numbers-In-JavaScript.js
@@ -27,3 +27,54 @@ console.log("x4 + y4 =", x4 + y4);
 console.log("\n");
 
 
+let x5 = 5;
+let y5 = 3;
+let z5 = x5 + y5;
+console.log("x5 =", x5, "\ny5 =", y5);
+console.log("x5 + y5 = z5 =>", z5);
+console.log("\n");
+
+
+let x6 = "100";
+let y6 = "10";
+console.log("x6 =", x6, "\ny6= ", y6);
+console.log("x6 / y6 =", x6 / y6);  
+console.log("x6 - y6 =", x6 - y6);
+console.log("x6 * y6 =", x6 * y6);
+console.log("x6 + y6 =", x6 + y6);
+console.log("\n");
+
+
+let x7 = "100";
+let y7 = 10;
+console.log("x7 =", x7, "\ny7 = ", y7);
+console.log("x7 / y7 =", x7 / y7);  
+console.log("x7 - y7 =", x7 - y7);  
+console.log("x7 * y7 =", x7 * y7);  
+console.log("x7 + y7 =", x7 + y7);  
+console.log("\n");
+
+
+let x8 = 10;
+let y8 = "Hello";
+console.log("x8 =", x8, "\ny8 = ", y8);
+console.log("x8 + y8 =", x8 + y8);
+console.log("x8 / y8 =", x8 / y8);
+console.log("typeOf NaN :", typeof NaN);
+console.log("\n");
+
+
+let x9 = 10;
+let y9 = 0;
+console.log("x9 =", x9, "\ny9 = ", y9);
+console.log("x9 + y9 =", x9 + y9);
+console.log("x9 / y9 =", x9 / y9);
+console.log("\n");
+
+
+let x10 = 10;
+let y10 = new Number(100);
+let z10 = new Number(1000);
+console.log("x10 == y10 ? :", x10 == y10);
+console.log("y10 == z10 ? :", y10 == z10);
+console.log("\n");
