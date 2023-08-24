95d9ce50c2e70bb477935898866f9bb5bfee0795
RohitSenta <rohitsenta@gmail.com>
2023-08-24 17:56:17 +0530

    DOM - Events And Event-Listener

---
+++ DOM-Event-And-Event-Listener.js
@@ -0,0 +1,13 @@
+
+
+// Change Text 
+function changeText(text){
+    text.innerText = "Oooops! Text Changed"; 
+}
+
+// Display Date And Time
+function displayDate() {
+    let date = document.querySelector(".date");
+    date.innerText = Date();
+}
+
